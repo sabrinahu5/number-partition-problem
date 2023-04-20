582719cec38dcadee03d81adeaa84ce71fbb98a7
Sabrina Hu <yourgitemail@example.com>
2023-04-19 23:18:39 -0400

    changes

--- partition.cpp
+++ partition.cpp
@@ -7,6 +7,7 @@
 #include <time.h>
 #include <math.h>
 #include <cmath>
+#include <ctime>
 #include "maxheap.hpp"
 
 #include <random>
@@ -14,11 +15,37 @@
 
 using namespace std;
 
-// implementation of Karmarkar-Karp algorithm
-// given sequence of n nonneg integers, want to output n +- 1's to minimize the residue
-// KK algorithm takes 2 largest elements in A and differencing them, replacing one with the difference and the other with 0
+// HELPER METHODS
 
-long kk(MaxHeap h) {
+// generates a random sequence of 1, -1 as a random solution
+std::vector<int> generateRandSol(int n) {
+
+    std::vector<int> sol(n);
+
+    for (int i = 0; i < n; i++) {
+        int set = rand() & 1;
+		sol[i] = (set != 1) ? -1 : 1;
+    }
+
+    return sol;
+
+}
+
+long residue(std::vector<long> nums, std::vector<int> sol, int n) 
+{
+    long res = 0;
+    for (int i = 0; i < n; i++) 
+    {
+        res += sol[i] * nums[i];
+    }
+    return abs(res);
+}
+
+
+// HEURISTICS
+
+// Karmarkar Karp's algorithm
+long karmarkarKarp(MaxHeap h) {
     while (h.size() > 1) {
         long a = h.extractMax();
         long b = h.extractMax();
@@ -27,7 +54,52 @@ long kk(MaxHeap h) {
     return h.extractMax();
 }
 
+// repeated random heuristic
+long repeatedRandom(std::vector<long> testvec) {
+    
+    int n = testvec.size();
 
+    std::vector<int> sol = generateRandSol(n);
+
+    for (int i = 0; i < 25000; i++) {
+        std::vector<int> sol2 = generateRandSol(n);
+        if (residue(testvec, sol2, n) < residue(testvec, sol, n)) {
+            sol = sol2;
+        }
+    }
+
+    return residue(testvec, sol, n);
+
+}
+
+long hillClimbing(MaxHeap h) {
+
+}
+
+long simulatedAnnealing(MaxHeap h) {
+
+}
+
+void test() {
+
+    MaxHeap test;
+    std::vector<long> testvec;
+    srand(time(0));
+
+    for (int i = 0; i < 100; i++) {
+
+        long range = 1000000000000LL; // set the range [1, 10^12]
+        long randNum = rand() % range + 1;
+
+        test.insert(randNum);
+        testvec.push_back(randNum);
+
+    }
+
+    long n = karmarkarKarp(test);
+    std::cout << n << std::endl;
+
+}
 
 
 int main(int argc, char* argv[]) {
@@ -40,17 +112,6 @@ int main(int argc, char* argv[]) {
         return 1;
     }*/
 
-    MaxHeap H;
-
-    H.insert(7);
-    H.insert(5);
-    H.insert(10);
-    H.insert(8);
-    H.insert(6);
-    long n = kk(H);
-    std::cout << n << std::endl;
-
-
-
+    test();
 
 }
